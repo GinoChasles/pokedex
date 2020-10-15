75220b5a538c341199b741b6379fd64f50984bc0
GinoChasles <gino.chasles@gmail.com>
2020-10-15 14:39:25 +0200

    test

--- script.js
+++ script.js
@@ -6,58 +6,97 @@ const api = "https://pokeapi.co/api/v2/pokemon?limit=150";
 /**
  * Try to parse a response as JSON data
  */
-function transformToJson (response) {
-    if (response.ok) {
-        return response.json();
-    }
+function transformToJson(response) {
+  if (response.ok) {
+    return response.json();
+  }
 
-    throw Error("Content not loaded");
+  throw Error("Content not loaded");
 }
 
 /**
  * Clear the list of all its items
  */
-function emptyList () {
-    // ...
+function emptyList() {
+  // ...
+  list.innerHTML = "";
+
+  // const empty = list.removeChild(description);
 }
 
 /**
  * Create an item, fetch its data and setup event listener
  */
-function createItem (pokemon) {
-    // Create a li tag
-    const item = document.createElement("li");
-    // ...
-    fetch(pokemon.url).then(transformToJson).then((data) => {
-        // ...
+function createItem(pokemon) {
+  // Create a li tag
+  const item = document.createElement("div");
+  // ...
+
+  const name = document.createElement("li");
+
+  const image = document.createElement("img");
+  list.appendChild(item);
+  item.appendChild(image);
+  item.appendChild(name);
+
+  fetch(pokemon.url)
+    .then(transformToJson)
+    .then((data) => {
+      // ...
+
+      image.src = data.sprites.front_default;
+      name.innerHTML = data.name;
+
+      item.addEventListener("click", () => {
+        showDescription(data);
+      });
     });
 }
 
 /**
  * fill the item list with values
  */
-function fillList (json) {
-    emptyList();
-    json.results.forEach(createItem);
+function fillList(json) {
+  emptyList();
+  json.results.forEach(createItem);
 }
 
 /**
  * Fill and display the description
  */
-function showDescription (data) {
-    description.classList.add("show");
+function showDescription(data) {
+  description.classList.add("show");
 
-    const fields = description.querySelectorAll("dd");
-    fields.forEach((dd) => {
-        // ...
-    });
+  const fields = description.querySelectorAll("dd");
+
+  fields.forEach((dd) => {
+    // ...
+    // const image = document.createElement("img");
+
+    // image.src = data.sprites.front_default;
+    // description.innerHTML = data.image;
+
+    dd.innerHTML = "";
+    const info = dd.className;
+
+    if (info == "types") {
+      dd.innerHTML = "";
+      data.types.forEach((type) => {
+        dd.innerHTML += "<div class='type'>" + type.type.name + "</div>";
+      });
+    } else {
+      dd.innerHTML = data[info];
+    }
+    console.log(dd);
+  });
 }
+document.querySelector(".close").addEventListener("click", hideDescription);
 
 /**
  * Hide the description
  */
-function hideDescription () {
-    description.classList.remove("show");
+function hideDescription() {
+  description.classList.remove("show");
 }
 
 // Fetch the API end-point and fill the list
